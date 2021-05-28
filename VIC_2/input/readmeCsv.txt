60c3097a544625765bf3ecd8324d787da871d708
Tim Wilson <timedwilson@gmail.com>
2021-05-28 13:05:07 +1000

    Copy .nls from VIC and everything else from Pakistan.

---
+++ VIC_2/input/readmeCsv.txt
@@ -0,0 +1,64 @@
+=== population.csv ===
+This file defines the characteristics of the agents in the model. There are always 2500 agents, which scale to each represent 10^n people in the population for n \in {0, 1, 2, 3, 4}. Each row is a cohort of agents that have the same basic characteristics.
+
+Many parameters are a boolean, which should be indicated by a value of 1 or 0.
+
+ - totalAgents       sets the size of the cohort. This column must sum to 2500
+ - age               sets the singular age of the cohort.
+ - atsi              sets whether the cohort are aborigional or torris strait islanders.
+ - disability        sets whether the cohort has a disability.
+ - essentialCount    is how many workers in the cohort are the most essential workers.
+ - workerCount       is how many agents for the cohort are considered to be working age.
+ - phase             is the phase in which the cohort is vacinated.
+ - subPhase          is the sub phase of vaccination. Ie the 'a' or 'b' after 1a or 1b
+ - spanMult          is a multiplier to the speed of the cohort.
+ - gatherFreqMult    is a multiplier to how often the cohort gathers at central locations.
+ - region            is the region index of the cohort. See region.csv
+ - succeptibleMult   is a multiplier for the chance of being infected.
+ - asymptomPropMult  is a multiplier to the base proportion of the population that is asymptomatic.
+ - vaccinatedCount   is the number of members of the cohort that start vaccinated.
+ - ignoreUptakeBoost ignores the effect of uptakeBoost. See vaccination.
+ - studentCount      is how many members of the cohort are students.
+
+A worker being essential causes the cohort to have the highest priority for going to work during a lockdown. For example, say we are in a lockdown where the policy sets 25% of workers to be regarded as essential. If only 15% of the total workers are in an 'essential' cohort, then all of the essential cohort works, with the remaining 10% made up of other workers.
+
+alreadyVaccinated does nothing for the moment, but will be used to set the number of initially vacciated members of the cohort.
+The comment column does nothing in the model.
+
+=== region.csv ===
+This file configures the properties of cohorts from different regions. Simulants only live together if they are from the same region.
+
+ - region            the region index. Corresponds to the value in population.csv.
+ - start             lower y coordinate for the region position. Simulants only set their houses within this band.
+ - end               upper y coordinate for the band within which simulants of the region have houses.
+ - houseSize         mean house size for this region.
+ - houseSizeRange    distance to the upper and lower bounds from the mean for house size in this region.
+
+Note that regions can overlap so this could be used to control the house sizes for cohorts. As with cohorts, try to avoid creating too many regions as the approximations required to squeeze the 2500 simulants into appropriate fractions become increasingly questionable.
+
+=== vaccine.csv ===
+This file configures the phase structure of the simulation. Currently the population in the D column had better match the sums of the populations in each corresponding phase and subPhase in population.csv.
+
+ - phase             is the phase.
+ - subPhase          is the letter after the phase, but as a number.
+ - days              is the number of days spent in this phase.
+ - name              is the name of the vaccine in this phase. Different names can use different model parameters.
+ - fixedSpeed        can be set to 1 to make the phase ignore param_vac_rate_mult. Otherwise set to 0.
+ - uptakeBoost       overrides uptake and vaccinates previous vaccine refusers.
+
+The parameter uptakeBoost ofen only makes sense near the end of the rollout. It gives cohorts a chance to boost their vaccination rate, unless the cohort has (ignoreUptakeBoost = 1). To do this it overrides param_vac_uptake (if higher) and adds simulants with (vaccinated = 0 and vaccineOffered = 1 and sm_vac_uptake < global_vaccineUptakeOverride) to the population to be vaccinated by this phase by giving them (vaccineOffered = 0). Cohorts in this phase use global_vaccineUptakeOverride as their uptake (if higher), unless they have (ignoreUptakeBoost = 1).
+
+The comment column is for human consumption. It does nothing in the model.
+
+The rate of vaccination is calculated by the model, and is simply set to population/days. This many vaccines will be offered to agents each day. This value does not need to be an integer, as fractional vaccine is carried between days, avoiding the issues that may arise with small intergers.
+
+=== incursion.csv ===
+This file sets the rate of incursions.
+
+ - days              is how long this phase of incursion settings lasts
+ - incursionScale    is a multiplier for the probability of any given infected arrival infecting someone in the community.
+ - infectedArrivals  is the number of oppotunities availible to infect a member of the community, per day.
+ - variant           is the variant (a number 1, 2 etc) that is brought in by incursions.
+ - varReplaceChance  is the probability of the new variant being spontaneously created when a simulant infects another simulant.
+
+The purpose of varReplaceChance is to introduce the new variant at a set time without worrying about the scale issues around incursions.
---
+++ VIC_2/processRcalc.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 12:28:02 2021
+
+@author: wilsonte
+"""
+
+import pandas as pd
+from matplotlib import pyplot
+import matplotlib.ticker as ticker
+import seaborn as sns
+#from tqdm import tqdm
+
+def Process(path, name):
+    df = pd.read_csv(path + name + '.csv', header=6)
+    df = df[['rand_seed', '[step]', 'average_R', 'global_transmissibility']]
+    lastStep = df['[step]'].max()
+    df = df[df['[step]'] == lastStep]
+    df = df[['rand_seed', 'average_R_all_regions', 'global_transmissibility']]
+    
+    df = df.set_index(['rand_seed', 'global_transmissibility'])
+    
+    df = df.unstack(level=-1)
+    df.columns = df.columns.get_level_values(1)
+    df.to_csv(path + name + '_process.csv')
+    df.describe().to_csv(path + name + '_metric.csv')
+    print(df.describe())
+
+
+def AddIfVaryingValue(colName, df, desiredIndex, toUnstack):
+    inTest = (colName in df.columns and (len(df[colName].unique()) > 1))
+    if inTest:
+        desiredIndex.append(colName)
+        toUnstack = toUnstack + 1
+        #df['testName'] = df['testName'].str.replace('EssWork', 'Ework')
+    else:
+        df = df.drop(columns=[colName])
+    return df, desiredIndex, toUnstack
+
+
+def ProcessVariableEnd(path, nameList, metricName):
+    name = nameList[0]
+    interestingColumns = [
+        'rand_seed', metricName, 'param_policy', 
+        'global_transmissibility', 'totalEndCount', 'slopeAverage',
+        'trackAverage', 'infectedTrackAverage', 'testName',
+        'gather_location_count',
+    ]
+    df = pd.DataFrame(columns=interestingColumns)
+    for v in nameList:
+        pdf = pd.read_csv(path + v + '.csv', header=6)
+        if 'testName' not in pdf.columns:
+            interestingColumns = filter(lambda x: x != 'testName', interestingColumns)
+        pdf = pdf[interestingColumns]
+        df  = df.append(pdf)
+    
+    desiredIndex = ['rand_seed', 'param_policy', 'global_transmissibility']
+    toUnstack = 1
+    
+    df, desiredIndex, toUnstack = AddIfVaryingValue('testName', df, desiredIndex, toUnstack)
+    df, desiredIndex, toUnstack = AddIfVaryingValue('gather_location_count', df, desiredIndex, toUnstack)
+    
+    if 'testName' in desiredIndex:
+        df['testName'] = df['testName'].str.replace('Is0.8 Stage3', 'Is0.75 Stage3')  
+    
+    df = df.set_index(desiredIndex)
+    df.to_csv(path + name + '_merge.csv')
+    
+    # Sometimes the random numbers collide.
+    df = df[~df.index.duplicated(keep='first')]
+    
+    for i in range(toUnstack):
+        df = df.unstack(level=-1)
+    
+    df.to_csv(path + name + '_process.csv')
+    df_desc = df.describe()
+    df_desc.to_csv(path + name + '_metric.csv')
+    
+    #print((df[metricName] * df['totalEndCount']).sum() / df['totalEndCount'].sum())
+    print(df.describe())
+    
+
+def MakePlot(df, varName,
+        topLevelFilter=False,
+        yTop=False,
+        yDomain=(-0.2, 9.2),
+        ymajticks=False,
+        yminticks=False,
+        hlines=False,
+        figWidth=48.5,
+        figHeight= 40,
+        ):
+    
+    if yTop:
+        yDomain = (-0.2, yTop - 0.8)
+        ymajticks = range(yTop)
+        yminticks = [i/5 for i in range(int(5*yTop))]
+    
+    xLabel = df.columns.names[1]
+    transmit_vals = list(dict.fromkeys([v[1] for v in df.columns]))
+    policy_vals = list(dict.fromkeys([v[2] for v in df.columns]))
+    #print(transmit_vals)
+
+    dataCount = len(df.columns)
+    
+    sns.set_theme(style="ticks", palette="pastel")
+    sns.set_style("ticks", {"xtick.major.size": 60})
+    
+    fig, ax = pyplot.subplots(figsize=(figWidth, figHeight))
+    plt = sns.boxplot(data=df, fliersize=1.8, showmeans=True,
+                      meanprops={"marker":"+","markerfacecolor":"black", "markeredgecolor":"black"})
+    #plt = sns.swarmplot(data=df, color=".25")
+    plt.set(xlim=(-1, dataCount + 1), ylim=yDomain)
+    sns.despine(ax=ax, offset=10)
+    
+    plt.set_xticklabels([''] * dataCount)
+    plt.xaxis.set_major_locator(ticker.FixedLocator([i*len(policy_vals) - 0.5 for i in range(len(transmit_vals))]))
+    plt.xaxis.set_minor_locator(ticker.FixedLocator([(i + 0.5)*len(policy_vals) - 0.5 for i in range(len(transmit_vals))]))
+    plt.xaxis.set_minor_formatter(ticker.FixedFormatter(transmit_vals))
+    
+    for tick in ax.xaxis.get_minor_ticks():
+        tick.label.set_fontsize(32) 
+        tick.label.set_rotation(45)
+        tick.label.set_horizontalalignment('right')
+        tick.label.set_verticalalignment('top')
+        tick.tick1line.set_markersize(0)
+        tick.tick2line.set_markersize(0)
+    
+    for tick in ax.yaxis.get_major_ticks():
+        tick.label.set_fontsize(32) 
+        
+    pyplot.xlabel(xLabel, fontsize=48)
+    pyplot.ylabel(varName, fontsize=48)
+    
+    if ymajticks:
+        ax.set_yticks(ymajticks)
+    if yminticks:
+        ax.set_yticks(yminticks, minor=True)
+    
+    if hlines:
+        for v in hlines:
+            ax.axhline(y=v, linewidth=2.2, zorder=0, color='r')
+    
+    ax.grid(which='minor', alpha=0.4, linewidth=1.5, zorder=-1, axis="y")
+    ax.grid(which='major', alpha=0.7, linewidth=2, zorder=-1)
+
+
+def ProcessToPlot(path, name, varName,
+        indexDepth=3,
+        indexReorder=False
+        ): 
+    df = pd.read_csv(path + name + '.csv', index_col=0,
+                     header=list(range(indexDepth)), skipinitialspace=True)
+    
+    unwantedTop = list(dict.fromkeys([v[0] for v in df.columns if v[0] != varName]))
+    df = df.drop(unwantedTop, axis=1, level=0)
+    
+    #df.columns = df.columns.set_levels(df.columns.levels[1].astype(int), level=1)
+                          
+    if indexReorder:
+        df.columns = df.columns.reorder_levels(indexReorder)
+        df.sort_values(['testName', 'housetotal', ],axis=1,inplace=True)
+    
+    df.to_csv(path + name + '_plot.csv')
+    df.describe().to_csv(path + name + '_plot_metric.csv')
+    return df
+
+nameNumber = '_5'
+namePath = 'runCalibrate'
+#nameStr = 'COVID SIMULS VIC JAN Vaccination Model R test 7-table' + str(nameNumber)
+#nameStr = 'headless MainCalibrate-table' + nameNumber
+nameStr = 'calibrate_stages_111'
+
+#nameStr = 'calibrate_stages_92' # This is the last large run of current params.
+
+#namePath = 'R regress'
+#nameStr = '55566792746ada8e5fd4b6c8efe14d2c736ad9f1_change'
+namePath = 'output/calibrate/'
+#namePath = 'output/' + namePath + '/'
+
+#metric_name = 'average_R_region_2'
+metric_name = 'average_R_all_regions'
+
+#MakePlot('output/' + namePath + '/', nameStr + '_process', 'slopeAverage',
+#    yDomain=(-0.3, 0.3),
+#    ymajticks=[i/10 - 0.3 for i in range(7)],
+#    yminticks=[i/50 - 0.3 for i in range(35)]
+#)
+#MakePlot(ProcessToPlot(
+#        'output/' + namePath + '/', nameStr + '_process',
+#        'average_R_all_regions',
+#        indexDepth=5,
+#        indexReorder=[0, 2, 1, 3, 4],
+#    ),
+#    'average_R_all_regions',
+#    yTop=5,
+#    hlines=[1, 2.5, 2.5*1.25, 2.5*1.5],
+#    width=60,
+#    #='1100',
+#)
+ProcessVariableEnd(namePath, [nameStr], metric_name)
+MakePlot(ProcessToPlot(
+        namePath, nameStr + '_process',
+        metric_name,
+        indexDepth=4,
+    ),
+    metric_name,
+    yTop=4,
+    hlines=[1, 2.5],
+    figWidth=30,
+    figHeight=20,
+)
+#MakePlot('output/' + namePath + '/', nameStr + '_process', 'trackAverage',
+#    yDomain=(0, 1),
+#    ymajticks=[i/5 for i in range(5)],
+#    yminticks=[i/25 for i in range(25)]
+#)
+#MakePlot('output/' + namePath + '/', nameStr + '_process', 'infectedTrackAverage',
+#    yDomain=(0, 1),
+#    ymajticks=[i/5 for i in range(5)],
+#    yminticks=[i/25 for i in range(25)]
+#)
