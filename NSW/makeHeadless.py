3fd87a8bb2a17019eae0d1c45f7b9881d1dc8f34
Tim Wilson <timedwilson@gmail.com>
2021-07-26 11:54:36 +1000

    Whitespace.

--- NSW/makeHeadless.py
+++ NSW/makeHeadless.py
@@ -6,28 +6,28 @@ Created on Thu Feb 11 12:11:03 2021
 """
 
 def MakeHeadless():
-    modelFile = open('COVID SIMULS VIC JAN Vaccination Model.nlogo', 'r')
-    outputFile = open('headless.nlogo', 'w')
-    ignore = False
-    
-    while True:
-        # Get next line from file
-        line = modelFile.readline()
-        if line == '':
-            break
-        line = line.rstrip()
-        
-        if line == 'MONITOR':
-            ignore = True
-        elif line == 'PLOT':
-            ignore = True
-        elif line == '':
-            ignore = False
-            
-        if not ignore:
-            outputFile.write(line + '\n')
-    
-    outputFile.close()
-    modelFile.close()
+	modelFile = open('COVID SIMULS VIC JAN Vaccination Model.nlogo', 'r')
+	outputFile = open('headless.nlogo', 'w')
+	ignore = False
+	
+	while True:
+		# Get next line from file
+		line = modelFile.readline()
+		if line == '':
+			break
+		line = line.rstrip()
+		
+		if line == 'MONITOR':
+			ignore = True
+		elif line == 'PLOT':
+			ignore = True
+		elif line == '':
+			ignore = False
+			
+		if not ignore:
+			outputFile.write(line + '\n')
+	
+	outputFile.close()
+	modelFile.close()
 
 MakeHeadless()
--- NSW/processRcalc.py
+++ NSW/processRcalc.py
@@ -12,158 +12,158 @@ import seaborn as sns
 #from tqdm import tqdm
 
 def Process(path, name):
-    df = pd.read_csv(path + name + '.csv', header=6)
-    df = df[['rand_seed', '[step]', 'average_R', 'global_transmissibility']]
-    lastStep = df['[step]'].max()
-    df = df[df['[step]'] == lastStep]
-    df = df[['rand_seed', 'average_R_all_regions', 'global_transmissibility']]
-    
-    df = df.set_index(['rand_seed', 'global_transmissibility'])
-    
-    df = df.unstack(level=-1)
-    df.columns = df.columns.get_level_values(1)
-    df.to_csv(path + name + '_process.csv')
-    df.describe().to_csv(path + name + '_metric.csv')
-    print(df.describe())
+	df = pd.read_csv(path + name + '.csv', header=6)
+	df = df[['rand_seed', '[step]', 'average_R', 'global_transmissibility']]
+	lastStep = df['[step]'].max()
+	df = df[df['[step]'] == lastStep]
+	df = df[['rand_seed', 'average_R_all_regions', 'global_transmissibility']]
+	
+	df = df.set_index(['rand_seed', 'global_transmissibility'])
+	
+	df = df.unstack(level=-1)
+	df.columns = df.columns.get_level_values(1)
+	df.to_csv(path + name + '_process.csv')
+	df.describe().to_csv(path + name + '_metric.csv')
+	print(df.describe())
 
 
 def AddIfVaryingValue(colName, df, desiredIndex, toUnstack):
-    inTest = (colName in df.columns and (len(df[colName].unique()) > 1))
-    if inTest:
-        desiredIndex.append(colName)
-        toUnstack = toUnstack + 1
-        #df['testName'] = df['testName'].str.replace('EssWork', 'Ework')
-    else:
-        df = df.drop(columns=[colName])
-    return df, desiredIndex, toUnstack
+	inTest = (colName in df.columns and (len(df[colName].unique()) > 1))
+	if inTest:
+		desiredIndex.append(colName)
+		toUnstack = toUnstack + 1
+		#df['testName'] = df['testName'].str.replace('EssWork', 'Ework')
+	else:
+		df = df.drop(columns=[colName])
+	return df, desiredIndex, toUnstack
 
 
 def ProcessVariableEnd(path, nameList, metricName):
-    name = nameList[0]
-    interestingColumns = [
-        'rand_seed', metricName, 'param_policy', 
-        'global_transmissibility', 'totalEndCount', 'testName',
-        'gather_location_count',
-    ]
-    df = pd.DataFrame(columns=interestingColumns)
-    for v in nameList:
-        pdf = pd.read_csv(path + v + '.csv', header=6)
-        if 'testName' not in pdf.columns:
-            interestingColumns = filter(lambda x: x != 'testName', interestingColumns)
-        pdf = pdf[interestingColumns]
-        df  = df.append(pdf)
-    
-    desiredIndex = ['rand_seed', 'param_policy', 'global_transmissibility']
-    toUnstack = 1
-    
-    df, desiredIndex, toUnstack = AddIfVaryingValue('testName', df, desiredIndex, toUnstack)
-    df, desiredIndex, toUnstack = AddIfVaryingValue('gather_location_count', df, desiredIndex, toUnstack)
-    
-    if 'testName' in desiredIndex:
-        df['testName'] = df['testName'].str.replace('Is0.8 Stage3', 'Is0.75 Stage3')  
-    
-    df = df.set_index(desiredIndex)
-    df.to_csv(path + name + '_merge.csv')
-    
-    # Sometimes the random numbers collide.
-    df = df[~df.index.duplicated(keep='first')]
-    
-    for i in range(toUnstack):
-        df = df.unstack(level=-1)
-    
-    df.to_csv(path + name + '_process.csv')
-    df_desc = df.describe()
-    df_desc.to_csv(path + name + '_metric.csv')
-    
-    #print((df[metricName] * df['totalEndCount']).sum() / df['totalEndCount'].sum())
-    print(df.describe().transpose()[['count', '50%', 'mean']])
-    
+	name = nameList[0]
+	interestingColumns = [
+		'rand_seed', metricName, 'param_policy', 
+		'global_transmissibility', 'totalEndCount', 'testName',
+		'gather_location_count',
+	]
+	df = pd.DataFrame(columns=interestingColumns)
+	for v in nameList:
+		pdf = pd.read_csv(path + v + '.csv', header=6)
+		if 'testName' not in pdf.columns:
+			interestingColumns = filter(lambda x: x != 'testName', interestingColumns)
+		pdf = pdf[interestingColumns]
+		df  = df.append(pdf)
+	
+	desiredIndex = ['rand_seed', 'param_policy', 'global_transmissibility']
+	toUnstack = 1
+	
+	df, desiredIndex, toUnstack = AddIfVaryingValue('testName', df, desiredIndex, toUnstack)
+	df, desiredIndex, toUnstack = AddIfVaryingValue('gather_location_count', df, desiredIndex, toUnstack)
+	
+	if 'testName' in desiredIndex:
+		df['testName'] = df['testName'].str.replace('Is0.8 Stage3', 'Is0.75 Stage3')  
+	
+	df = df.set_index(desiredIndex)
+	df.to_csv(path + name + '_merge.csv')
+	
+	# Sometimes the random numbers collide.
+	df = df[~df.index.duplicated(keep='first')]
+	
+	for i in range(toUnstack):
+		df = df.unstack(level=-1)
+	
+	df.to_csv(path + name + '_process.csv')
+	df_desc = df.describe()
+	df_desc.to_csv(path + name + '_metric.csv')
+	
+	#print((df[metricName] * df['totalEndCount']).sum() / df['totalEndCount'].sum())
+	print(df.describe().transpose()[['count', '50%', 'mean']])
+	
 
 def MakePlot(df, varName,
-        topLevelFilter=False,
-        yTop=False,
-        yDomain=(-0.2, 9.2),
-        ymajticks=False,
-        yminticks=False,
-        hlines=False,
-        figWidth=48.5,
-        figHeight= 40,
-        ):
-    
-    if yTop:
-        yDomain = (-0.2, yTop - 0.8)
-        ymajticks = range(yTop)
-        yminticks = [i/5 for i in range(int(5*yTop))]
-    
-    xLabel = df.columns.names[1]
-    transmit_vals = list(dict.fromkeys([v[1] for v in df.columns]))
-    policy_vals = list(dict.fromkeys([v[2] for v in df.columns]))
-    #print(transmit_vals)
+		topLevelFilter=False,
+		yTop=False,
+		yDomain=(-0.2, 9.2),
+		ymajticks=False,
+		yminticks=False,
+		hlines=False,
+		figWidth=48.5,
+		figHeight= 40,
+		):
+	
+	if yTop:
+		yDomain = (-0.2, yTop - 0.8)
+		ymajticks = range(yTop)
+		yminticks = [i/5 for i in range(int(5*yTop))]
+	
+	xLabel = df.columns.names[1]
+	transmit_vals = list(dict.fromkeys([v[1] for v in df.columns]))
+	policy_vals = list(dict.fromkeys([v[2] for v in df.columns]))
+	#print(transmit_vals)
 
-    dataCount = len(df.columns)
-    
-    sns.set_theme(style="ticks", palette="pastel")
-    sns.set_style("ticks", {"xtick.major.size": 60})
-    
-    fig, ax = pyplot.subplots(figsize=(figWidth, figHeight))
-    plt = sns.boxplot(data=df, fliersize=1.8, showmeans=True,
-                      meanprops={"marker":"+","markerfacecolor":"black", "markeredgecolor":"black"})
-    #plt = sns.swarmplot(data=df, color=".25")
-    plt.set(xlim=(-1, dataCount + 1), ylim=yDomain)
-    sns.despine(ax=ax, offset=10)
-    
-    plt.set_xticklabels([''] * dataCount)
-    plt.xaxis.set_major_locator(ticker.FixedLocator([i*len(policy_vals) - 0.5 for i in range(len(transmit_vals))]))
-    plt.xaxis.set_minor_locator(ticker.FixedLocator([(i + 0.5)*len(policy_vals) - 0.5 for i in range(len(transmit_vals))]))
-    plt.xaxis.set_minor_formatter(ticker.FixedFormatter(transmit_vals))
-    
-    for tick in ax.xaxis.get_minor_ticks():
-        tick.label.set_fontsize(32) 
-        tick.label.set_rotation(45)
-        tick.label.set_horizontalalignment('right')
-        tick.label.set_verticalalignment('top')
-        tick.tick1line.set_markersize(0)
-        tick.tick2line.set_markersize(0)
-    
-    for tick in ax.yaxis.get_major_ticks():
-        tick.label.set_fontsize(32) 
-        
-    pyplot.xlabel(xLabel, fontsize=48)
-    pyplot.ylabel(varName, fontsize=48)
-    
-    if ymajticks:
-        ax.set_yticks(ymajticks)
-    if yminticks:
-        ax.set_yticks(yminticks, minor=True)
-    
-    if hlines:
-        for v in hlines:
-            ax.axhline(y=v, linewidth=2.2, zorder=0, color='r')
-    
-    ax.grid(which='minor', alpha=0.4, linewidth=1.5, zorder=-1, axis="y")
-    ax.grid(which='major', alpha=0.7, linewidth=2, zorder=-1)
-    pyplot.show()
+	dataCount = len(df.columns)
+	
+	sns.set_theme(style="ticks", palette="pastel")
+	sns.set_style("ticks", {"xtick.major.size": 60})
+	
+	fig, ax = pyplot.subplots(figsize=(figWidth, figHeight))
+	plt = sns.boxplot(data=df, fliersize=1.8, showmeans=True,
+					  meanprops={"marker":"+","markerfacecolor":"black", "markeredgecolor":"black"})
+	#plt = sns.swarmplot(data=df, color=".25")
+	plt.set(xlim=(-1, dataCount + 1), ylim=yDomain)
+	sns.despine(ax=ax, offset=10)
+	
+	plt.set_xticklabels([''] * dataCount)
+	plt.xaxis.set_major_locator(ticker.FixedLocator([i*len(policy_vals) - 0.5 for i in range(len(transmit_vals))]))
+	plt.xaxis.set_minor_locator(ticker.FixedLocator([(i + 0.5)*len(policy_vals) - 0.5 for i in range(len(transmit_vals))]))
+	plt.xaxis.set_minor_formatter(ticker.FixedFormatter(transmit_vals))
+	
+	for tick in ax.xaxis.get_minor_ticks():
+		tick.label.set_fontsize(32) 
+		tick.label.set_rotation(45)
+		tick.label.set_horizontalalignment('right')
+		tick.label.set_verticalalignment('top')
+		tick.tick1line.set_markersize(0)
+		tick.tick2line.set_markersize(0)
+	
+	for tick in ax.yaxis.get_major_ticks():
+		tick.label.set_fontsize(32) 
+		
+	pyplot.xlabel(xLabel, fontsize=48)
+	pyplot.ylabel(varName, fontsize=48)
+	
+	if ymajticks:
+		ax.set_yticks(ymajticks)
+	if yminticks:
+		ax.set_yticks(yminticks, minor=True)
+	
+	if hlines:
+		for v in hlines:
+			ax.axhline(y=v, linewidth=2.2, zorder=0, color='r')
+	
+	ax.grid(which='minor', alpha=0.4, linewidth=1.5, zorder=-1, axis="y")
+	ax.grid(which='major', alpha=0.7, linewidth=2, zorder=-1)
+	pyplot.show()
 
 
 def ProcessToPlot(path, name, varName,
-        indexDepth=3,
-        indexReorder=False
-        ): 
-    df = pd.read_csv(path + name + '.csv', index_col=0,
-                     header=list(range(indexDepth)), skipinitialspace=True)
-    
-    unwantedTop = list(dict.fromkeys([v[0] for v in df.columns if v[0] != varName]))
-    df = df.drop(unwantedTop, axis=1, level=0)
-    
-    #df.columns = df.columns.set_levels(df.columns.levels[1].astype(int), level=1)
-                          
-    if indexReorder:
-        df.columns = df.columns.reorder_levels(indexReorder)
-        df.sort_values(['testName', 'housetotal', ],axis=1,inplace=True)
-    
-    df.to_csv(path + name + '_plot.csv')
-    df.describe().to_csv(path + name + '_plot_metric.csv')
-    return df
+		indexDepth=3,
+		indexReorder=False
+		): 
+	df = pd.read_csv(path + name + '.csv', index_col=0,
+					 header=list(range(indexDepth)), skipinitialspace=True)
+	
+	unwantedTop = list(dict.fromkeys([v[0] for v in df.columns if v[0] != varName]))
+	df = df.drop(unwantedTop, axis=1, level=0)
+	
+	#df.columns = df.columns.set_levels(df.columns.levels[1].astype(int), level=1)
+						  
+	if indexReorder:
+		df.columns = df.columns.reorder_levels(indexReorder)
+		df.sort_values(['testName', 'housetotal', ],axis=1,inplace=True)
+	
+	df.to_csv(path + name + '_plot.csv')
+	df.describe().to_csv(path + name + '_plot_metric.csv')
+	return df
 
 
 nameNumber = '_5'
@@ -201,15 +201,15 @@ metric_name = 'average_R_all_regions'
 #)
 ProcessVariableEnd(namePath, [nameStr], metric_name)
 MakePlot(ProcessToPlot(
-        namePath, nameStr + '_process',
-        metric_name,
-        indexDepth=4,
-    ),
-    metric_name,
-    yTop=9,
-    hlines=[1, 5.5, 6.5],
-    figWidth=30,
-    figHeight=20,
+		namePath, nameStr + '_process',
+		metric_name,
+		indexDepth=4,
+	),
+	metric_name,
+	yTop=9,
+	hlines=[1, 5.5, 6.5],
+	figWidth=30,
+	figHeight=20,
 )
 #MakePlot('output/' + namePath + '/', nameStr + '_process', 'trackAverage',
 #    yDomain=(0, 1),
