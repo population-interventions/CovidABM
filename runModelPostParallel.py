8b2c398b579aa2915f0bc61ed8afc21888abb7a8
Tim Wilson <timedwilson@gmail.com>
2022-04-29 13:21:25 +1000

    Fix.

--- runModelPostParallel.py
+++ runModelPostParallel.py
@@ -1,8 +1,8 @@
 import sys
 
-runIndex = int(sys.argv[2]) if len(sys.argv) > 1 else 1
-modelName = sys.argv[1] if len(sys.argv) > 2 else 'rc_auto'
-runs = sys.argv[2] if len(sys.argv) > 3 else 10
-pernode = sys.argv[3] if len(sys.argv) > 4 else 5
+runIndex = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+modelName = sys.argv[2] if len(sys.argv) > 2 else 'rc_auto'
+runs = int(sys.argv[3]) if len(sys.argv) > 3 else 10
+pernode = int(sys.argv[4]) if len(sys.argv) > 4 else 5
 
 print(modelName, runIndex)
