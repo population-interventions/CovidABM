d4ec459a8b9f913ba0fe0a8a1057d155f9601d8b
Tim Wilson <timedwilson@gmail.com>
2022-06-07 14:52:08 +1000

    Fiddle with succept.

--- runModelParallelPost.py
+++ runModelParallelPost.py
@@ -7,5 +7,4 @@ runs = int(sys.argv[3]) if len(sys.argv) > 3 else 10
 pernode = int(sys.argv[4]) if len(sys.argv) > 4 else 2
 
 print('DoParallelCheck', runIndex, modelName, runs, pernode)
-for i in range(40):
-	DoParallelCheck(i + 1, modelName, runs, pernode)
+DoParallelCheck(runIndex, modelName, runs, pernode)
--- runModelSetup.py
+++ runModelSetup.py
@@ -3,7 +3,7 @@ import setup.modelSetup as modelSetup
 import setup.splitInput as splitInput
 
 modelName = sys.argv[1] if len(sys.argv) > 1 else 'stage_auto'
-runCount = int(sys.argv[2]) if len(sys.argv) > 2 else 2
+runCount = int(sys.argv[2]) if len(sys.argv) > 2 else 400
 cullUi = (len(sys.argv) > 2)
 
 modelSetup.MakeHeadlessWithParameters(modelName, runCount=runCount, cullUi=cullUi)
