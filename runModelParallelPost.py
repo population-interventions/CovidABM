7d5b87a9a1a3509f08dd68cce4b7c14ab12e35b9
Tim Wilson <timedwilson@gmail.com>
2022-07-04 16:43:49 +1000

    Whitespace.

--- runModelParallelPost.py
+++ runModelParallelPost.py
@@ -4,8 +4,8 @@ from process.checkParallel import DoParallelCheck
 #import warnings
 #warnings.simplefilter(action = "error", category = RuntimeWarning)
 
-runIndex = int(sys.argv[1]) if len(sys.argv) > 1 else 3
-modelName = sys.argv[2] if len(sys.argv) > 2 else 'vic_main_small'
+runIndex = int(sys.argv[1]) if len(sys.argv) > 1 else 1
+modelName = sys.argv[2] if len(sys.argv) > 2 else 'vic_main'
 runs = int(sys.argv[3]) if len(sys.argv) > 3 else 10
 pernode = int(sys.argv[4]) if len(sys.argv) > 4 else 2
 
--- runModelSetup.py
+++ runModelSetup.py
@@ -4,7 +4,7 @@ import setup.splitInput as splitInput
 
 modelName = sys.argv[1] if len(sys.argv) > 1 else 'vic_main'
 runCount = int(sys.argv[2]) if len(sys.argv) > 2 else 1
-cullUi = (len(sys.argv) > 2)
+cullUi = (len(sys.argv) > 2) or True
 
 modelSetup.MakeHeadlessWithParameters(modelName, runCount=runCount, cullUi=cullUi)
 
