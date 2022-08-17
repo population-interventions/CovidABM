0f0389c599b145237aa6b8cedee01a8e98e60a42
Tim Wilson <timedwilson@gmail.com>
2022-08-17 16:58:57 +1000

    Tweak?

--- runModelSetup.py
+++ runModelSetup.py
@@ -2,7 +2,7 @@ import sys
 import setup.modelSetup as modelSetup
 import setup.splitInput as splitInput
 
-modelName = sys.argv[1] if len(sys.argv) > 1 else 'vic_mask'
+modelName = sys.argv[1] if len(sys.argv) > 1 else 'vic_mask_stage2'
 runCount = int(sys.argv[2]) if len(sys.argv) > 2 else 1
 cullUi = (len(sys.argv) > 2) or True
 
