b24829e637f53498642c15d1b72bfd4ecd4f443a
Tim Wilson <timedwilson@gmail.com>
2022-10-17 15:44:09 +1100

    Increase stage calc range.

--- runModelSetup.py
+++ runModelSetup.py
@@ -2,9 +2,9 @@ import sys
 import setup.modelSetup as modelSetup
 import setup.splitInput as splitInput
 
-modelName = sys.argv[1] if len(sys.argv) > 1 else 'rc_auto'
+modelName = sys.argv[1] if len(sys.argv) > 1 else 'stage_auto'
 runCount = int(sys.argv[2]) if len(sys.argv) > 2 else 1
-cullUi = (len(sys.argv) > 2) or True
+cullUi = (len(sys.argv) > 2) or False
 
 modelSetup.MakeHeadlessWithParameters(modelName, runCount=runCount, cullUi=cullUi)
 
