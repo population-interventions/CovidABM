6d433297f65ca4721ed95509b83a1dc55a0346ca
Tim Wilson <timedwilson@gmail.com>
2022-11-29 16:04:45 +1100

    Fix.

--- runModelParallelPost.py
+++ runModelParallelPost.py
@@ -5,9 +5,9 @@ from process.checkParallel import DoParallelCheck
 #warnings.simplefilter(action = "error", category = RuntimeWarning)
 
 runIndex = int(sys.argv[1]) if len(sys.argv) > 1 else 1
-modelName = sys.argv[2] if len(sys.argv) > 2 else 'stage_auto'
-runs = int(sys.argv[3]) if len(sys.argv) > 3 else 10
-pernode = int(sys.argv[4]) if len(sys.argv) > 4 else 2
+modelName = sys.argv[2] if len(sys.argv) > 2 else 'vic_mask_stage2_square'
+runs = int(sys.argv[3]) if len(sys.argv) > 3 else 500
+pernode = int(sys.argv[4]) if len(sys.argv) > 4 else 1
 
 print('DoParallelCheck', runIndex, modelName, runs, pernode)
 DoParallelCheck(runIndex, modelName, runs, pernode)
